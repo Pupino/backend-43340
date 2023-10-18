8c959822f8be83696564aa53f03106b8d088cba3
Romina Jalón <romina.jalon@gmail.com>
2023-10-17 22:01:26 -0300

    Delete ProyectoFinal directory

--- ProyectoFinal/src/routes/views.router.js
+++
@@ -1,8 +0,0 @@
-//@ts-check
-import express from 'express';
-export const viewsRouter = express.Router();
-import { viewsController } from '../controllers/views.controller.js';
-
-viewsRouter.get('/', viewsController.home);
-viewsRouter.get('/login', viewsController.login);
-viewsRouter.get('/logout', viewsController.logout);
